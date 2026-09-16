5cb3b1b819709a4a964e5859f2abfd2606d9f170
Yash Gupta <yash12722@gmail.com>
2026-09-16 09:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (92.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // if(s.length() != t.length()) {
+        //     return false ;
+        // }
+
+        // sort(s.begin() , s.end()) ;
+        // sort(t.begin() , t.end()) ;
+        // if(s== t) {
+        //     return true  ;
+        // }
+        // return false ;
+
+        // approach 2 : using asci value
         if(s.length() != t.length()) {
             return false ;
         }
 
-        sort(s.begin() , s.end()) ;
-        sort(t.begin() , t.end()) ;
-        if(s== t) {
-            return true  ;
-        }
-        return false ;
+        int count[26] = {0} ;
+        for(char ch : s ) 
+            count[ch - 97]++ ;
+        for(char ch : t) 
+            count[ch - 97]-- ;
+        for(int val : count)
+            if(val != 0)
+                return false;
+        return true ;
+
     }
 };
